29b36a1570f500bb370764e02568c195e6b70188
mcgallucci <42854238+manuelcgallucci@users.noreply.github.com>
2025-02-11 00:01:12 -0300

    feat: done part I

--- challenge/model.py
+++ challenge/model.py
@@ -1,13 +1,25 @@
 import pandas as pd
+import xgboost as xgb
+import numpy as np
 
 from typing import Tuple, Union, List
+from datetime import datetime
 
 class DelayModel:
 
     def __init__(
         self
     ):
-        self._model = None # Model should be saved in this attribute.
+        self._model = xgb.XGBClassifier(random_state=1, learning_rate=0.01, scale_pos_weight=4.440)
+
+    def _get_min_diff(data: pd.Dataframe) -> pd.DataFrame:
+        """
+        Get minimum difference of datetimes for delay calculations
+        """
+        fecha_o = datetime.strptime(data['Fecha-O'], '%Y-%m-%d %H:%M:%S')
+        fecha_i = datetime.strptime(data['Fecha-I'], '%Y-%m-%d %H:%M:%S')
+        min_diff = ((fecha_o - fecha_i).total_seconds())/60
+        return min_diff
 
     def preprocess(
         self,
@@ -26,7 +38,36 @@ class DelayModel:
             or
             pd.DataFrame: features.
         """
-        return
+        top_10_features = [
+            "OPERA_Latin American Wings", 
+            "MES_7",
+            "MES_10",
+            "OPERA_Grupo LATAM",
+            "MES_12",
+            "TIPOVUELO_I",
+            "MES_4",
+            "MES_11",
+            "OPERA_Sky Airline",
+            "OPERA_Copa Air"
+        ]
+        features = pd.concat([
+            pd.get_dummies(data['OPERA'], prefix = 'OPERA'),
+            pd.get_dummies(data['TIPOVUELO'], prefix = 'TIPOVUELO'), 
+            pd.get_dummies(data['MES'], prefix = 'MES')], 
+            axis = 1
+        )
+        features = features[top_10_features]
+
+
+        if target_column is not None:
+            threshold_in_minutes = 15
+
+            min_diff = data.apply(self._get_min_diff, axis = 1)
+            targets = pd.DataFrame(np.where(min_diff > threshold_in_minutes, 1, 0), [target_column])
+
+            return features, targets
+
+        return features
 
     def fit(
         self,
@@ -40,7 +81,7 @@ class DelayModel:
             features (pd.DataFrame): preprocessed data.
             target (pd.DataFrame): target.
         """
-        return
+        self._model.fit(features, target)
 
     def predict(
         self,
@@ -55,4 +96,5 @@ class DelayModel:
         Returns:
             (List[int]): predicted targets.
         """
-        return
+        predictions = self._model.predict(features)
+        return predictions.tolist()
---
+++ docs/challenge.md
@@ -0,0 +1,11 @@
+
+
+## Part I
+
+- Fixing exploration.ipynb graphs bugs
+- Choosing the model which has better perfomance (balanced and with only top10 features)
+- Typically XGBoost is preferable on larger datasets with non-linear and more complex relations (non-linear). In this case some features are correlated so the patterns are better captured by the XGBoost modle.
+
+- Writing model.py, the method _get_min_diff was added from the collab, which should also be tested in test_model.py.
+- The preprocessing is done by doing one-hot encoding and taking the top10 features. Targets are calculated the same way as the ipynb. 
+- The fit is done directly using the features and targets. Here, we asume the train/test split is done somewhere else.
